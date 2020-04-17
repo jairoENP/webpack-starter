5750d5df23abd962f834c8045225bfa70cdc70e6
Jairo <jairoverachaly@gmail.com>
2020-04-17 14:43:29 -0400

    Primer commit

---
+++ src/js/componentes.js
@@ -0,0 +1,11 @@
+import '../css/componentes.css';
+
+
+export const saludar = (nombre) => {
+    console.log('creando etiqueta H1, en el html');
+
+    const h1 = document.createElement('h1');
+    h1.innerHTML = `Hola ${nombre}`;
+
+    document.body.append(h1);
+}
